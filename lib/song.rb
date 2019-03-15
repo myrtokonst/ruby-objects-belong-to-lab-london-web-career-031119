2302810899d4765d03283877bd148706b11d9dbb
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-15 12:05:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,9 @@
 class Song 
   attr_accessor :title, :artist
-  
+  @@all = []
   def initialize 
     @title = title 
     @artist = artist
-  end 
-  
-  def artist
-    
+    @@all << self
   end
-  
-  
 end 
